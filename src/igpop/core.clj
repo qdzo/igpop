f3d0908512b608fe3ac8381731283034e87da6ba
qdzo <banch.idea19@gmail.com>
2019-12-21 14:56:11 +0300

    Try to redesign run method.More pure,testable,extendable (DRAFT)

--- src/igpop/core.clj
+++ src/igpop/core.clj
@@ -66,3 +66,57 @@
 
 
   )
+
+;; -----------------------------------------------------------------------------------------
+
+(defmulti run2 (fn [nm & args] (keyword nm)))
+
+(defn help-msg []
+  (str "igpop [cmd] [subcmd] opts\n"
+       (->> (methods run2)
+            (remove (comp #{:help :default} key))
+            (map (fn [[k f]] (str k " - " (second (f)))))
+            (interpose "\n")
+            (apply str))))
+
+(defmethod run2
+  :help
+  [_ & args]
+  [:print "very concise usage guide"])
+
+(help-msg)
+
+(defmethod run2
+  :default
+  [& args]
+  (if-let [cmd (first args)]
+    [:print (str "No such command - " cmd "\n" (help-msg))]
+    [:print (help-msg)]))
+
+(defmethod run2
+  :validate
+  [& args]
+  [:print (apply str "Validate..." args)])
+
+(defmethod run2
+  :build
+  [& args]
+  (if (second args)
+    [:site/build (System/getProperty "user.dir") (second args)]
+    [:print "USAGE: igpop build {your_baseurl} EXAMPLE: igpop build /igpop"]))
+
+(defmethod run2
+  :dev
+  [& args]
+  (let [i (.indexOf (or args []) "-p")
+        port (last (butlast args))]
+    (cond
+      (and (> i 0) (not (= "-p" port)))
+      [:site/start (System/getProperty "user.dir") (Integer. port)]
+      (and (< i 0) (= "dev" port))
+      [:site/start (System/getProperty "user.dir") 8899]
+      :else
+      [:print "-p to setup a port (default is 8899)"])))
+
+(defn -main2 [& args]
+  (run2 args))
